a66e4bc085ce395bf8516410455e5bff6fa714ae
lucas <lucas@debian.debian.org.br>
2023-09-19 13:32:21 -0300

    added i3_arch_install script

---
+++ i3_arch_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "---------------------------------------------------------"
+echo "Bem-vindo ao Instalador Automático do Lucolesco!"
+echo "Será instalado o Arch linux com i3-gaps!"
+echo "---------------------------------------------------------"
+	sleep 2
+echo "---------------------------------------------------------"
+echo "Primeiro, iremos particionar o disco."
+echo "---------------------------------------------------------"
+sleep 1
+echo "\n"
+echo "---------------------------------------------------------"
+echo "Esses são todos os discos atualmente em seu PC:"
+echo "---------------------------------------------------------"
+sudo fdisk -l | grep "Dis"
+echo "---------------------------------------------------------"
+echo "Com essas informações, digite qual será o disco escolhido:"
+read disco
+echo "---------------------------------------------------------"
+fdisk $disco
+
