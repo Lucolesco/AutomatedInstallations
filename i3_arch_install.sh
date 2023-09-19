d8320146cd70e33f3d03a3b197187d5af32e1180
Lucolesco <lucas564lith@gmail.com>
2023-09-19 15:06:55 -0300

    third try

--- i3_arch_install.sh
+++ i3_arch_install.sh
@@ -50,7 +50,8 @@ echo "Gerando FSTAB (configuração do sistema)"
 echo "------------------------------------------------------------"
 sleep 1
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt | (
+
+(
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 echo 'pt_BR.UTF-8 UTF-8' > /etc/locale.gen
@@ -101,8 +102,7 @@ cp -a .config/* /home/$nome_de_usuario/.config/
 
 systemctl enable NetworkManager
 systemctl enable gdm
-exit
-)
+exit ) | arch-chroot /mnt
 
 sleep 3
 echo "----------------------------------------------------------------"
